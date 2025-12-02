b7ced446193151a1eae400ccaae128a83a0588c9
Jason Yau <jasonyau0130@gmail.com>
2025-12-02 14:55:31 +0800

    strong casting int to double

--- demo-datatype/src/DemoMath.java
+++ demo-datatype/src/DemoMath.java
@@ -2,7 +2,7 @@ public class DemoMath {
   public static void main(String[] args) {
     // operators
     int remainder = 10 % 3;
-    System.out.println(remainder);
+    System.out.println("remainder: " + remainder);
 
     // int + double
     double sum = 3 + 3.5;
@@ -13,5 +13,16 @@ public class DemoMath {
     short s10 = 9;
     // short s20 = b10 + s10
     int i20 = b10 + s10;
+    System.out.println(i20);
+
+    // calculate average score
+    int englishScore = 87;
+    int mathScore = 72;
+    int historyScore = 34;
+
+    double averageScore =
+        (double) (englishScore + mathScore + historyScore) / 3;
+
+    System.out.println("averageScore: " + averageScore);
   }
 }
