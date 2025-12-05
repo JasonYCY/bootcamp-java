c9db76c1860f34b53572c0e1589efc025fc737d0
Jason Yau <jasonyau0130@gmail.com>
2025-12-05 10:29:01 +0800

    comment out last example

--- demo-datatype/src/DemoLoop.java
+++ demo-datatype/src/DemoLoop.java
@@ -238,16 +238,16 @@ public class DemoLoop {
     // }
 
     // print * triangle
-    System.out.println();
-    for (int i = 0; i < 4; i++) {
-      for (int j = 0; j <= i; j++) {
-        System.out.print(" ");
-      }
-      for (int j = 0; j < (4 - i) * 2 - 1; j++) {
-        System.out.print("*");
-      }
-      System.out.println();
-    }
+    // System.out.println();
+    // for (int i = 0; i < 4; i++) {
+    // for (int j = 0; j <= i; j++) {
+    // System.out.print(" ");
+    // }
+    // for (int j = 0; j < (4 - i) * 2 - 1; j++) {
+    // System.out.print("*");
+    // }
+    // System.out.println();
+    // }
 
   }
 }
