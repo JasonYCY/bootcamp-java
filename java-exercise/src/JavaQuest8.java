92b49a540284ddb0686034f1059fe8370a483780
Jason Yau <jasonyau0130@gmail.com>
2025-12-08 15:48:43 +0800

    JavaQuest8

---
+++ java-exercise/src/JavaQuest8.java
@@ -0,0 +1,47 @@
+public class JavaQuest8 {
+  /**
+   * Write a loop to find the second max number.
+   * Example Output for testCase1:
+   * Second Max = 230
+   */
+  public static int findSecondMax(int[] arr) {
+    int max = arr[0];
+    for (int i = 1; i < arr.length; i++) {
+      if (arr[i] > max) {
+        max = arr[i];
+        arr[i] = Integer.MIN_VALUE;
+      }
+    }
+    
+    int secondMax = Integer.MIN_VALUE;
+    for (int i = 1; i < arr.length; i++) {
+      if (arr[i] > secondMax) {
+        secondMax = arr[i];
+      }
+    }
+
+    return secondMax;
+  }
+  
+  public static void main(String[] args) {
+    int[] testCase1 = new int[] {-10, 5, 100, 240, 230, 80}; // Second Max = 230
+    int[] testCase2 = new int[] {-10, 5, 100, 120, 240, 200}; // Second Max = 200
+    int[] testCase3 = new int[] {-10, 5, 120, -100, 100, 240}; // Second Max = 120
+    int[] testCase4 = new int[] {-10, 5, 100, 240, 240, 80}; // Second Max = 240
+
+    // Your program should be able to handle all the above test case.
+    int secondMax;
+    // code here ...
+    secondMax = findSecondMax(testCase1);
+    System.out.println("(Test Case 1) Second Max = " + secondMax);
+
+    secondMax = findSecondMax(testCase2);
+    System.out.println("(Test Case 2) Second Max = " + secondMax);
+
+    secondMax = findSecondMax(testCase3);
+    System.out.println("(Test Case 3) Second Max = " + secondMax);
+
+    secondMax = findSecondMax(testCase4);
+    System.out.println("(Test Case 4) Second Max = " + secondMax);
+  }
+}
