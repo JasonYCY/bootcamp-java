82c2d6dca40c302625b0b3023f50ab98b5a9561f
Jason Yau <jasonyau0130@gmail.com>
2025-12-10 16:29:41 +0800

    add more constructors for Circle class

--- demo-class-object/src/Circle.java
+++ demo-class-object/src/Circle.java
@@ -3,18 +3,25 @@ import java.math.BigDecimal;
 public class Circle {
   // attribute
   private double radius;
+
+  // constructor
+  public Circle() {}
+
+  public Circle(double radius) {
+    this.radius = radius;
+  }
   
   // method
+  public double area() {
+    return BigDecimal.valueOf(Math.PI).multiply(BigDecimal.valueOf(radius).pow(2)).doubleValue();
+  }
+
   public double getRadius() {
     return radius;
   }
 
   public void setRadius(double radius) {
     this.radius = radius;
-  }
-
-  public double area() {
-    return BigDecimal.valueOf(Math.PI).multiply(BigDecimal.valueOf(radius).pow(2)).doubleValue();
   }
 
   // main method
@@ -25,6 +32,9 @@ public class Circle {
     c1.setRadius(3.5);
     System.out.println(c1.getRadius()); // 3.5
     System.out.println(c1.area()); // 38.48451000647496
+
+    Circle c2 = new Circle(1.5);
+    System.out.println(c2.getRadius());
 
 
 
