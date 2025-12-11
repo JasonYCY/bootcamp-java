27da0cc7ec31c5eb3b2172c7734559d9c1664768
Jason Yau <jasonyau0130@gmail.com>
2025-12-11 17:35:31 +0800

    update shuffle methods in ShuffleHelper

--- demo-class-object/src/ShuffleHelper.java
+++ demo-class-object/src/ShuffleHelper.java
@@ -3,32 +3,65 @@ import java.util.Random;
 public class ShuffleHelper {
   // attribute
   private Card[] cards;
+  private Random random;
 
   // constructor
   public ShuffleHelper(Card[] cards) {
     this.cards = cards;
+    this.random = new Random();
   }
 
   // method
-  public void shuffle(int shuffleNumber) {
-    Random random = new Random();
-    int randomCardIndex;
-
-    for (int i = 0; i < shuffleNumber; i++) {
-      randomCardIndex = random.nextInt(52);
-      Card pickedCard = cards[randomCardIndex];
-      Card temp = cards[0];
-      cards[0] = pickedCard;
-      cards[randomCardIndex] = temp;
+  // Method 1: Proper Fisher-Yates shuffle (standard algorithm)
+  public void shuffle() {
+    // Start from the last element and swap with random element
+    for (int i = cards.length - 1; i > 0; i--) {
+      int j = random.nextInt(i + 1); // Random index from 0 to i
+      
+      // Swap cards[i] with cards[j]
+      Card temp = cards[i];
+      cards[i] = cards[j];
+      cards[j] = temp;
     }
   }
+
+  // Method 2: Do multiple Fisher-Yates shuffle (multiple passes)
+  public void shuffleMultipleTimes(int numberOfPasses) {
+    for (int pass = 0; pass < numberOfPasses; pass++) {
+      // Do one Fisher-Yates shuffle per pass
+      for (int i = cards.length - 1; i > 0; i--) {
+        int j = random.nextInt(i + 1);
+        
+        Card temp = cards[i];
+        cards[i] = cards[j];
+        cards[j] = temp;
+      }
+    }
+  }
+
+  // Method 3: Random Pairwise Swaps
+  public void shuffleWithSwaps(int numberOfSwaps) {
+    for (int i = 0; i < numberOfSwaps; i++) {
+      int index1 = random.nextInt(cards.length);
+      int index2 = random.nextInt(cards.length);
+      
+      // Only swap if they're different
+      if (index1 != index2) {
+        Card temp = cards[index1];
+        cards[index1] = cards[index2];
+        cards[index2] = temp;
+      }
+    }
+  }
+
+
 
   // main method
   public static void main(String[] args) {
     // test
     Deck deck = new Deck();
     ShuffleHelper helper = new ShuffleHelper(deck.getCards());
-    helper.shuffle(1000);
+    helper.shuffleMultipleTimes(3);
     System.out.println(deck);
 
 
