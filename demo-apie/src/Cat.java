f04b38bcce43f5757da28ca9bd55296b56d28004
Jason Yau <jasonyau0130@gmail.com>
2025-12-15 15:05:02 +0800

    update Cat class

--- demo-apie/src/Cat.java
+++ demo-apie/src/Cat.java
@@ -1,3 +1,11 @@
 public class Cat extends Animal {
-  
+  private String hair;
+
+  public String getHair() {
+    return hair;
+  }
+
+  public void setHair(String hair) {
+    this.hair = hair;
+  }
 }
