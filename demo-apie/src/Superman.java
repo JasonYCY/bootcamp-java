7a317638b7a1813dcf22eabd3911f21b8b003c7a
Jason Yau <jasonyau0130@gmail.com>
2025-12-16 11:44:39 +0800

    Superman now implements the Flyable interface

--- demo-apie/src/Superman.java
+++ demo-apie/src/Superman.java
@@ -1,10 +1,11 @@
-public class Superman extends Human {
+public class Superman extends Human implements Flyable {
   // Super Class: Class or Abstract Class
   // Human has no abstract method
   public Superman(String name) {
     super(name);
   }
 
+  // implementing interface ensures that an object can fly during compile time
   public void fly() {
 
   }
