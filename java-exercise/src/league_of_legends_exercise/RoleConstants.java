6c605577a1a3f5289c828a11590de1cf721b2219
Jason Yau <jasonyau0130@gmail.com>
2025-12-17 11:26:41 +0800

    update format

--- java-exercise/src/league_of_legends_exercise/RoleConstants.java
+++ java-exercise/src/league_of_legends_exercise/RoleConstants.java
@@ -11,9 +11,38 @@ public record RoleConstants(
   double criticalChance,          // Warrior: 0.05, Mage: 0.15, Archer: 0.20
   double criticalDamageMultiplier // Warrior: 2.5, Mage: 1.8, Archer: 1.5
 ) {
-  public static final RoleConstants warrior = new RoleConstants(1.5, 0.7, 1.3, 1.4, 0.6, 0.7, 0.8, 0.05, 2.50);
-  public static final RoleConstants mage = new RoleConstants(1.0, 1.5, 0.8, 0.9, 1.4, 1.3, 0.6, 0.15, 1.8);
-  public static final RoleConstants archer = new RoleConstants(1.2, 1.2, 1.1, 1.0, 1.0, 1.0, 1.5, 0.20, 1.5);
+  public static final RoleConstants warrior = new RoleConstants(
+    1.5,
+    0.7,
+    1.3,
+    1.4,
+    0.6,
+    0.7,
+    0.8,
+    0.05,
+    2.50);
+
+  public static final RoleConstants mage = new RoleConstants(
+    1.0,
+    1.5,
+    0.8,
+    0.9,
+    1.4,
+    1.3,
+    0.6,
+    0.15,
+    1.8);
+
+  public static final RoleConstants archer = new RoleConstants(
+    1.2,
+    1.2,
+    1.1,
+    1.0,
+    1.0,
+    1.0,
+    1.5,
+    0.20,
+    1.5);
 
   public static RoleConstants getConstants(Role role) {
     if (role == Role.WARRIOR)
