ae6355da34db5747c8026ebe9d84a06ccbbbfd05
Jason Yau <jasonyau0130@gmail.com>
2025-12-22 11:01:37 +0800

    implement toString equals hashCode method for Student class

--- demo-data-structure/src/Student.java
+++ demo-data-structure/src/Student.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Student {
   private String name;
   private int score;
@@ -21,5 +23,24 @@ public class Student {
 
   public void setScore(int score) {
     this.score = score;
+  }
+
+  @Override
+  public String toString() {
+    return "Student [name=" + name + ", score=" + score + "]";
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (!(obj instanceof Student)) return false;
+
+    Student student = (Student) obj;
+    return name.equals(student.getName()) && score == student.getScore();
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, score);
   }
 }
