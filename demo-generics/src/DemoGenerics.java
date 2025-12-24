03f2a7630691f4e9be4cb22c6e4db1055dcb810c
Jason Yau <jasonyau0130@gmail.com>
2025-12-24 11:40:30 +0800

    intro to generics

---
+++ demo-generics/src/DemoGenerics.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class DemoGenerics {
+  public static void main(String[] args) {
+    // Java generics
+    ArrayList<String> arr = new ArrayList<>();
+    System.out.println(arr);
+  }
+}
